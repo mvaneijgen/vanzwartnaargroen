d066e0afd5dd195a96a89927082f45ed46c3ea0f
Mitchel van Eijgen <mvaneijgen@gmail.com>
2019-10-31 20:32:01 +0100

    telephone=no

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,10 @@
 const routerBase =
   process.env.DEPLOY_ENV === "GH_PAGES"
     ? {
-        router: {
-          base: "/vanzwartnaargroen/"
-        }
+      router: {
+        base: "/vanzwartnaargroen/"
       }
+    }
     : {};
 
 module.exports = {
@@ -30,6 +30,7 @@ module.exports = {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: 'format-detection', content: 'telephone=no' },
       {
         name: "viewport",
         content:
