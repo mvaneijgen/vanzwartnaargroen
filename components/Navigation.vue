239f4ceca8762edd7ea669f7a195a1c7bb51dd80
Mitchel van Eijgen <mvaneijgen@gmail.com>
2019-10-31 20:47:59 +0100

    Removed back

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="component-Navigation">
     <nav>
-      <nuxt-link to="/start" v-if="$nuxt.$route.path != '/start'" class="alloy-back">Terug naar tool</nuxt-link>
       <nuxt-link to="/">Home</nuxt-link>
       <nuxt-link to="/start">Start</nuxt-link>
       <nuxt-link to="/about">About</nuxt-link>
